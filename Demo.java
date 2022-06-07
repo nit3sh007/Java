3579fea88420dbe4489b906bf5f6651a46bd4a55
smartnitesh <niteshkumar503@gmail.com>
2022-06-07 21:58:07 +0530

    data_type

---
+++ Demo.java
@@ -0,0 +1,18 @@
+class Demo {
+    public static void main(String[] args) {
+        int a=12200;
+        float b= 10.23f;
+        double c= 1111111;
+        String s= "nitesh";
+        boolean one= false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.print(c);
+        System.out.print("\n");
+        System.out.println(s);
+        System.out.println(one);
+
+        
+    }    
+}
---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+
+    static int m = 100;  
+    void method(){
+        int n =90;
+    }
+    public static void main(String args[]) {
+        int data= 50;
+    System.out.println(m);
+
+    }
+
+}
