e6f4e22d0a66d29284605a9556b8005a407ae99b
smartnitesh <niteshkumar503@gmail.com>
2022-06-08 21:31:19 +0530

    litrels

---
+++ Literals.java
@@ -0,0 +1,15 @@
+class Literals {
+
+    public static void main(String args[] ) {
+        int x =10;
+        int y=010; // converted in octal
+        int z=0*10;
+
+        System.out.println(x);
+        System.out.println(y);   //(10)8 = 0x8(0)+8(1) = 8
+        System.out.println(z);
+
+
+    }
+    
+}
