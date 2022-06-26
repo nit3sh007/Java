fff6fcf3911dd1884bedc95c2928954ce16c3f46
smartnitesh <niteshkumar503@gmail.com>
2022-06-26 18:51:51 +0530

    random_number

---
+++ Random_Number.java
@@ -0,0 +1,16 @@
+public class Random_Number {
+
+    public static void main(String args[]){
+    int max= 100;
+    int min = 200;
+    double a = Math.random()*(max-min+1)+min;  
+    System.out.println("random numer "+ Math.round(a));
+     //  System.out.println("random numer"+Math.random());
+    }
+
+
+
+     
+    }
+    
+
