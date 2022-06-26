f72b56c07b9a08a5c05957bd605a989a8737e49c
smartnitesh <niteshkumar503@gmail.com>
2022-06-26 17:35:45 +0530

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+import javax.print.event.PrintEvent;
+
+public class Fibonacci {
+      
+        public static void main(String args[])  
+        {    
+        
+            int n1=0,n2=1,n3,i,count=10;
+            System.out.print(n1 +" "+ n2); // print 0,1 
+            for(i=2;i<=count;i++){
+               n3=n1+n2;
+               System.out.print(" "+n3);
+               n1=n2;
+               n2=n3;
+
+            }
+
+
+          
+        }}  
+    
