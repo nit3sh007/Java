d1e89c71bd6852735bb8e2b012d64b1a6f9dba4c
smartnitesh <niteshkumar503@gmail.com>
2022-06-28 13:31:01 +0530

    Armstrong

---
+++ Arm2.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String args[]){
+        System.out.println("enter a nmber");
+        Scanner number = new Scanner(System.in);
+        int x = number.nextInt();
+
+        int temp =x;
+        int sum=0;
+        
+        while(x>0){
+            
+            int r=x%10;
+            sum= sum+(r*r*r);
+            x=x/10;
+        }
+        if(temp==sum){
+            System.out.println("armstrong");
+        } 
+        else{
+            System.out.println("not");
+          
+        }
+
+    }
+}
